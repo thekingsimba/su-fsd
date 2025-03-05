d3d3ef874b1367d75f0afe11c5a20af7102a837a
thekingsimba <thekingsimbadev@gmail.com>
2025-03-05 17:44:00 +0100

    (fix) : fix cors error on vercel

--- src/pages/api/file-content.js
+++ src/pages/api/file-content.js
@@ -2,6 +2,14 @@ import fs from 'fs';
 import path from 'path';
 
 export default async function handler(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    if (req.method === "OPTIONS") {
+      return res.status(200).end();
+    }
+    
     const filePath = path.join(process.cwd(), 'public', 'data.csv');
 
     try {
